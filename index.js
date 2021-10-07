2a2876b00cb32277316997f46096230ec97b0670
Franco Briones <ebrionesm17_1@unc.edu.pe>
2021-10-07 00:00:24 -0500

    Terminamos autenticacion y acceso de servicios

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ const app=express();
 
 //Configurar Cors
 app.use(cors());
+app.use(express.json());
 
 //Estableciendo conexion a la BD
 dbConection();
 //Rutas de la API proyecto
-app.get('/',(req,res)=>{
-    res.json({
-        ok : true,
-        msg : 'Bienvenido a Node.js'
-    });
-});
-
+app.use('/api/usuarios', require('./routes/usuarios.routes'));
+app.use('/api/login', require('./routes/auth.routes')); 
 //Codigo para desplegar el servidor
 app.listen(process.env.PORT,()=> {
     console.log('Servidor desplegado en el puerto:'+process.env.PORT);
---
+++ routes/usuarios.routes.js
@@ -0,0 +1,30 @@
+/*
+Path: /api/usuarios
+*/
+const {Router} = require('express');
+const {check} = require('express-validator');
+const {validarCampos} = require('../middlewares/validar-campos');
+const { getUsuarios, crearUsuario, actualizarUsuario,eliminarUsuario } = require('../Controllers/usuario.controller');
+const { validarJWT } = require ('../middlewares/validar-jwt');
+const router = Router();
+router.get('/', validarJWT, getUsuarios);
+router.post('/',[
+
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password','El password es obligatorio').not().isEmpty(),
+    check('email','El email es obligatorio').isEmail(),
+    validarCampos,
+] , crearUsuario);
+
+router.put('/:id',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email','El email es obligatorio').isEmail(),
+        //check('role','El rol es obligatorio').not().isEmpty(),
+        validarCampos,   
+    ] ,actualizarUsuario
+);
+router.delete('/:id',validarJWT,eliminarUsuario);
+
+module.exports = router;
